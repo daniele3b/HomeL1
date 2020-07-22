7c23de18cbb7658964b2527a52119f91941a9464
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-07-22 15:19:23 +0200

    added template generation

--- helper/markdownEdit.js
+++ helper/markdownEdit.js
@@ -5,28 +5,90 @@ function markdownEditFile(data) {
   if (data.lang == "eng") {
     fs.writeFile(
       config.get("tmp_location") + data.name + data.surname + ".md",
-      " --- \n name:" +
+      "--- \n name: " +
         data.name +
-        "\n surname:" +
+        "\n surname: " +
         data.surname +
-        "\n birthday:" +
-        data.birthday +
-        "\n day:" +
+        "\n birthday: " +
+        data.birthday_month +
+        "/" +
+        data.birthday_day +
+        "/" +
+        data.birthday_year +
+        "\n day: " +
+        data.month +
+        "/" +
         data.day +
-        "\n street:" +
+        "/" +
+        data.year +
+        "\n street: " +
         data.street +
-        "\n city:" +
+        "\n city: " +
         data.city +
-        "\n cap:" +
+        "\n cap: " +
         data.cap +
-        "\n to:" +
+        "\n to: " +
         data.to +
-        "\n ...",
+        "\n...\n",
       function (err) {
         if (err) throw err;
         console.log("File is created successfully.");
       }
     );
+
+    if (data.lang == "ita") {
+      fs.writeFile(
+        config.get("tmp_location") + data.name + data.surname + ".md",
+        "--- \n name: " +
+          data.name +
+          "\n surname: " +
+          data.surname +
+          "\n birthday: " +
+          data.birthday_day +
+          "/" +
+          data.birthday_month +
+          "/" +
+          data.birthday_year +
+          "\n day: " +
+          data.day +
+          "/" +
+          data.month +
+          "/" +
+          data.year +
+          "\n street: " +
+          data.street +
+          "\n city: " +
+          data.city +
+          "\n cap: " +
+          data.cap +
+          "\n to: " +
+          data.to +
+          "\n...\n",
+        function (err) {
+          if (err) throw err;
+          console.log("File is created successfully.");
+        }
+      );
+    }
+
+    //Creo il file md completo necessario
+    var w = fs.createWriteStream(
+      config.get("tmp_location") + data.name + data.surname + ".md",
+      { flags: "a" }
+    );
+
+    var r = fs.createReadStream(
+      config.get("template_location") +
+        config.get("template_name") +
+        data.lang +
+        ".md"
+    );
+
+    w.on("close", function () {
+      console.log("done writing");
+    });
+
+    r.pipe(w);
   }
 }
 
