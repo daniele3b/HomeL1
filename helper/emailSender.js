1be036643b184f25bce88dc6adad83159276dd46
Mario <cavaiola.1794586@studenti.uniroma1.it>
2020-07-23 11:57:23 +0200

    .env

--- helper/emailSender.js
+++ helper/emailSender.js
@@ -4,15 +4,14 @@ async function emailSender(pdf, data) {
   let info = transporter.sendMail({
     from: '"HomeLess Project" <progetto-diana@libero.it>',
     to: "mariocavaiola@libero.it",
-    subject: "That's your document!",
-    text: "Hi, that's your document!",
-    html: "Hi, that's your document!",
-    /*
+    subject: data.name+", that's your document!",
+    html: "Hi "+data-name+", that's your document!",
+    
         attachments: [
             {
-             path: ''
+             path: pdf
             }
-         ]*/
+         ]
   });
 }
 
--- startup/emailSender.js
+++ startup/emailSender.js
@@ -1,12 +1,15 @@
 const nodemailer = require("nodemailer");
+const config = require('config')
+require('dotenv').config()
+
 
 let transporter = nodemailer.createTransport({
     host: "smtp.libero.it",
     port: 465,
     secure: true, 
     auth: {
-      user: "", 
-      pass: ""
+      user: process.env.USER_EMAIL, 
+      pass: process.env.PW_EMAIL 
     }
 });
 
