9780fec4a220aa536f811d6d6e39fa7260c9d366
Mario <cavaiola.1794586@studenti.uniroma1.it>
2020-07-23 12:06:52 +0200

    d

--- helper/emailSender.js
+++ helper/emailSender.js
@@ -3,7 +3,7 @@ const { transporter } = require("../startup/emailSender");
 async function emailSender(pdf, data) {
   let info = transporter.sendMail({
     from: '"HomeLess Project" <progetto-diana@libero.it>',
-    to: "mariocavaiola@libero.it",
+    to: data.email,
     subject: data.name+", that's your document!",
     html: "Hi "+data-name+", that's your document!",
     
